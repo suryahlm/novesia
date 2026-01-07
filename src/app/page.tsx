a0f45eda42e81fca85cf1c9f0c7b956e6ea8234a
Novesia Developer <user@novesia.com>
2026-01-08 04:49:53 +0700

    Revert "Fix: Rating Tertinggi horizontal scroll"
    This reverts commit 6885a9c619c4846e1465f1571f0aca732792f42e.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -241,7 +241,7 @@ export default async function HomePage() {
               title="Rating Tertinggi"
               href="/discover?sort=rating"
             />
-            <div className="flex gap-4 overflow-x-auto no-scrollbar pb-2 -mx-4 px-4">
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
               {topRated.map((novel) => (
                 <BookCard
                   key={novel.id}
